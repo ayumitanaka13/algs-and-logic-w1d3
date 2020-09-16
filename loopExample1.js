15109ff3ea5f6a3f24000ee4b24cc72dfd3186d6
ayumitanaka <tanakayumi13@gmail.com>
2020-09-16 10:12:11 -0700

    first commit

---
+++ loopExample1.js
@@ -0,0 +1,20 @@
+for (let i = 0; i < 10; i++) {
+    if (i === 5) {
+        console.log("i is = 5")
+    } else {
+        console.log("i is not = 5");
+    }
+}
+
+/*
+i is not = 5
+i is not = 5
+i is not = 5
+i is not = 5
+i is not = 5
+i is = 5
+i is not = 5
+i is not = 5
+i is not = 5
+i is not = 5
+*/
